4f91994a6b9006870eea6db31d475ae521818af6
stefko3z <53911104+stefko3z@users.noreply.github.com>
2020-06-10 22:43:57 +0200

    Feature/map view (#4)
    * add MapView and initial MapHeader
    
    * add onRightClick hook to MapView and MapComponent
    
    * remove bootstrap dependency and refactor MapComponent.js
    
    Co-authored-by: Joana Becheva <joana.becheva@gmail.com>

---
+++ src/components/MapComponent.js
@@ -0,0 +1,26 @@
+import React from "react";
+import  { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+
+export default function MapComponent(props) {
+    /**
+     * withScriptjs - loads the Google Maps Javascript API v3
+     * withGoogleMap - initializes the map component
+     */
+    const MapComponent = withScriptjs(withGoogleMap((properties) =>
+        <GoogleMap
+            defaultZoom={8}
+            defaultCenter={{lat: 48.137154, lng: 11.576124}}
+            options={{disableDefaultUI: true}}
+            onRightClick={props.onRightClick}
+        />
+    ));
+
+    return(
+        <MapComponent
+            googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyC-1_iM_0fSsAenW9hkQSGcQU36f2k8HAU"}
+            loadingElement={<div style={{height: '100%'}}/>}
+            containerElement={<div style={{height: '100%'}}/>}
+            mapElement={<div style={{height: '100%'}}/>}
+        />
+    );
+}
---
+++ src/views/MapView.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { Grid, Drawer, makeStyles, Typography } from "@material-ui/core";
+
+import MapHeader from "../components/MapHeader";
+import MapComponent from "../components/MapComponent";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    mapHeader: {
+        flexGrow: 1
+    },
+    element: {
+        margin: theme.spacing(0),
+        padding: theme.spacing(0),
+        height: "100vh",
+        border: theme.palette.primary
+    },
+    menuElement: {
+        width: "15vw",
+        padding: theme.spacing(1)
+    }
+}));
+
+// View of mapview page
+export default function MapView() {
+
+    const classes = useStyles();
+
+    const [leftMenuOpen, setLeftMenu] = useState(false);
+    const [discussionOpen, setDiscussion] = useState(false);
+
+    // Callback functions for opening/closing leftsideMenu
+    const toggleLeftMenu = (open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+          return;
+        }
+        setLeftMenu(open);
+    };
+
+    const toggleDiscussion = (event) => {
+        // Do additional checks if required
+        console.log(event);
+        setDiscussion(!discussionOpen);
+    };
+
+    // Set container for map and disucssion pane
+    let grid;
+    if(discussionOpen) {
+        grid = (
+            <Grid container>
+                <Grid item xs={8} className={classes.element}>
+                    <MapComponent onRightClick={toggleDiscussion}/>
+                </Grid>
+                <Grid item xs={4} className={classes.element}>
+                    <Typography variant="h6" className={classes.menuElement}>
+                        Test
+                    </Typography>
+                </Grid>
+            </Grid>
+        );
+    } else {
+        grid = (
+            <Grid item xs={12} className={classes.element}>
+                <MapComponent onRightClick={toggleDiscussion}/>
+            </Grid>
+        );
+    }
+
+    return (<div className={classes.root}>
+        <MapHeader className={classes.mapHeader} position={"fixed"} onLeftMenuClick={toggleLeftMenu}/>
+        <Drawer anchor='left' open={leftMenuOpen} onClose={toggleLeftMenu(false)}>
+            <Typography variant="h6" className={classes.menuElement}>
+                Test
+            </Typography>
+        </Drawer>
+        {grid}
+    </div>);
+}
