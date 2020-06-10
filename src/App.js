22a29e8e8c403410268ed4abe6098172427ec01c
Joana Becheva <64614394+yonaip@users.noreply.github.com>
2020-06-10 19:07:04 +0200

    add map component and add material UI dependency (#3)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from "react-bootstrap";
-import {MapWrapper} from './views/Map.js';
+import Map from './components/Map.js';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import './App.css';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(0),
+        textAlign: 'center',
+        height: '100vh',
+        color: theme.palette.text.secondary,
+    },
+}));
+
 function App() {
-  return (
-      <Container fluid>
-        <Row>
-          <Col sm={8} className="Map">
-            {MapWrapper}
-          </Col>
-          <Col sm={4} className="Discussion">Discussion</Col>
-        </Row>
-      </Container>
+    const classes = useStyles();
+
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={0}>
+            <Grid item xs={8} className={classes.paper}>
+                <Map/>
+            </Grid>
+            <Grid item xs={4}>
+                Discussion
+            </Grid>
+        </Grid>
+      </div>
   );
 }
 
