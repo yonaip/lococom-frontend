3029563f5cba16af3d17aad664dda890708901a1
sylviapas <67117878+sylviapas@users.noreply.github.com>
2020-06-27 12:08:51 +0200

    Changed AddComment to Grid Style, Added Methods for reading Discussions and writing Comments

--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -1,43 +1,43 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import FilterHdrIcon from '@material-ui/icons/FilterHdr';
-import ToggleButton from '@material-ui/lab/ToggleButton';
-import Box from '@material-ui/core/Box';
 import requestimg from '../resources/request.png';
 import walkerimg from '../resources/shoes.png';
 import photoimg from '../resources/photograph.png';
 import natureimg from '../resources/nature.png';
 import alertimg from '../resources/alert.png';
 import axios from 'axios';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import { Grid, Button, TextField, Box, TextareaAutosize, Typography, Fab, ButtonBase, Avatar } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     background: '#CAE2E5',
-    height: '90vh',
-    marginLeft: theme.spacing(160),
-    marginTop: theme.spacing(10),
-    overflow: "hidden",
+    marginLeft: theme.spacing(2),
+    marginTop: theme.spacing(1),
+    flexGrow: 1,
   },
-  rectangle: {
-    background: '#B9CFD4',
-    height: '8vh',
-    
+  element: {
+    position: 'relative',
+    justifyContent: 'inherit',
   },
+  text: {
+    color: "black",
+    textAlign: 'left',
+    padding: theme.spacing(1),
+    flexGrow: 2,
+    marginBottom: "2%"
+  },
+
   icon: {
-    margin: theme.spacing(0.5,3),
     float: 'left',
   },
     user: {
         background: '#62AEBB',
         height: '5vh',
         width: '8vw',
-        margin: theme.spacing(2,0,0,0),
         borderRadius: theme.shape.borderRadius,
         color: "white",
         fontSize: 20,
@@ -46,76 +46,66 @@ const useStyles = makeStyles((theme) => ({
       },
       request: {
         backgroundColor:"#B5CDD0",
-        width: "4vw",
-        height: "4vw",
+        marginTop: "2%",
+        width: "3.5vw",
+        height: "3.5vw",
         borderRadius:"50%",
-        margin: "10px 20px 0px 20px",
         backgroundImage: 'url('+ requestimg+')',
         backgroundRepeat: "no-repeat",
         backgroundSize:"70%",
         backgroundPosition: "center",
-        float: 'left',
       },
       walking: {
         backgroundColor:"#B5CDD0",
-        width: "4vw",
-        height: "4vw",
+        marginTop: "2%",
+        width: "3.5vw",
+        height: "3.5vw",
         borderRadius:"50%",
-        margin: "10px 20px 0px 20px",
         backgroundImage: 'url('+ walkerimg+')',
         backgroundRepeat: "no-repeat",
         backgroundSize:"70%",
         backgroundPosition: "center",
-        float: 'left',
       },
       nature: {
         backgroundColor:"#B5CDD0",
-        width: "4vw",
-        height: "4vw",
+        marginTop: "2%",
+        width: "3.5vw",
+        height: "3.5vw",
         borderRadius:"50%",
-        margin: "10px 20px 0px 20px",
         backgroundImage: 'url('+ natureimg+')',
         backgroundRepeat: "no-repeat",
         backgroundSize:"70%",
         backgroundPosition: "center",
-        float: 'left',
       },
       photo: {
         backgroundColor:"#B5CDD0",
-        width: "4vw",
-        height: "4vw",
+        marginTop: "2%",
+        width: "3.5vw",
+        height: "3.5vw",
         borderRadius:"50%",
-        margin: "10px 20px 0px 20px",
         backgroundImage: 'url('+ photoimg+')',
         backgroundRepeat: "no-repeat",
         backgroundSize:"70%",
         backgroundPosition: "center",
-        float: 'left',
       },
-    
       hint: {
         backgroundColor:"#B5CDD0",
-        width: "4vw",
-        height: "4vw",
+        marginTop: "2%",
+        width: "3.5vw",
+        height: "3.5vw",
         borderRadius:"50%",
-        margin: "10px 20px 0px 20px",
         backgroundImage: 'url('+ alertimg+')',
         backgroundRepeat: "no-repeat",
         backgroundSize:"70%",
         backgroundPosition: "center",
-        float: 'left',
       },
-    discussion: {
-        fontSize: 30,
-        color: "black",
-        float: 'left',
-    },
 
     identifier: {
         fontSize: 15,
         color: '#706666',
         float: 'left',
-        margin: theme.spacing(0,0.5),
+        marginBottom: "2%"
+       /* margin: theme.spacing(0,0.5),*/
     },
     
     topic: {
@@ -130,55 +120,46 @@ const useStyles = makeStyles((theme) => ({
     },
     image: {
         background: "black",
-        margin:  theme.spacing(2,4),
-        width: '28vw',
+        /*margin:  theme.spacing(2,4),*/
+        width: '90%',
         height: '30vh', 
         float: 'left',
         color: "white",
+        marginTop: "2.5%",
         borderRadius: theme.shape.borderRadius,
     },
     comment: {
         background: '#62AEBB',
         textTransform: 'none',
-        minWidth: '20vw',
         textAlign: "left",
-        /*width: '30vw',*/
+        width: '60%',
         flexgrow: 1,
-        margin: theme.spacing(2,4),
-
+        marginTop: "2%",
         borderRadius: theme.shape.borderRadius,
         color: "black",
         fontSize: 15,
         float: 'left',
-        minHeight: "45px",
+        minHeight: "20px",
       },
 
-      send : {
-        float: 'left',
-        maxWidth: '60px',
-        
-    },
-    test: {
-            
-            margin:  theme.spacing(27,0),
+    newComment: {
             maxheight: '10px', 
             float: 'left',
             color: "white",
             borderRadius: theme.shape.borderRadius,
             overflow: "hidden",
-        
+            width: '80%',
+            marginTop: "45%",
+            background: "white",
     },
-    buttontest: {
-        margin:  theme.spacing(28,0,0,0),
-        marginLeft: "30px",
+    
+    post: {
         float: 'left',
+        marginTop: "45%",
+        marginLeft: "2.5%"
     },
-    
-    
   
 }));
-
-
 
 export default function AddComment(props) {
   const classes = useStyles();
@@ -187,7 +168,16 @@ export default function AddComment(props) {
   const [content, setContent] = React.useState("");
   const [topic, setTopic] = React.useState("");
   const [creator, setCreator] = React.useState("");
+  const [discussionId, setDiscussionId] = React.useState("");
+  const [user, setUser] = React.useState("User123");
+  const [commentcontent, setCommentContent] = React.useState("");
+  const [commentuser, setCommentUser] = React.useState(""); // just for testing, author of first comment shown on page
+  const [commenttext, setCommentText] = React.useState(""); // just for testing, Content of first comment shown on page
+
  React.useEffect(() => {
+   let test ="";
+  function getDiscussion(){
+    let id = "";
   axios
   .get("/discussion/getDiscussion")
   .then(({ data }) => {
@@ -196,74 +186,115 @@ export default function AddComment(props) {
    setContent(data[0].content);
    setTopic(data[0].topic);
    setCreator(data[0].username);
+   setDiscussionId(data[0]._id);
+    }); }
+   
+  function getComment(){
+    
+    axios
+    .post("/comment/getComment", {id: "5ef49b899e3feb20183b9126"}) // hardcoded, just for testing
+    .then(({ data }) => {
+      console.log(data);
+      setCommentUser(data[0].username);
+      setCommentText(data[0].content);
     });
+  }
+getDiscussion();
+getComment();
+
 }, []);
 
-
-
-
+const onChangeContent = (event) => {
+  setCommentContent(event.target.value);
+};
+// todo change
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const comment = {
+   username: user,
+   content: commentcontent,
+   discussionId: discussionId
+  };
+  axios
+  .post('/comment/createComment', comment)
+  .then(response => {console.log('Comment created')})
+  .catch(err => {
+    console.error(err);
+  });
+  
+};
 
 
   return (
-    <div>
-    <div className={classes.root}>
-      <div className={classes.rectangle}>
+    <Grid container className={classes.root} justify="space-around">
+      <Grid item xs={12} className={classes.element}>
+        <ButtonBase>
+          <AccountCircleIcon className={classes.icon} color="disabled" style={{ fontSize: 65 }}/>
+          <Typography variant="h6" className={classes.text}>
+            {user}
+          </Typography>
+        </ButtonBase>
+      </Grid>
       
-      <AccountCircleIcon className={classes.icon} color="disabled" style={{ fontSize: 65 }}/>
-      <div className={classes.user}>
-        UserName
-    </div>
-    </div>
-    <Button variant="outlined" className={topic === 'Nature' ? classes.nature : topic === 'Request' ? classes.request : topic === 'Walking' ? classes.walking : topic === 'Photo' ? classes.photo : topic === 'Hint' ? classes.hint : classes.hint}> 
-    </Button>
-    <div className={classes.discussion}>{title}</div>
-    <div className={classes.identifier}>
-            Posted by {creator}
+  
+
+        <Grid item xs={12} className={classes.element}>
+        <ButtonBase>
+        <Button variant="outlined" className={topic === 'Nature' ? classes.nature : topic === 'Request' ? classes.request : topic === 'Walking' ? classes.walking : topic === 'Photo' ? classes.photo : topic === 'Hint' ? classes.hint : classes.hint}> </Button>
+          <Typography variant="h6" className={classes.text}>
+          Posted by {creator} <br/> {title}
+          </Typography>
+        </ButtonBase>
+      </Grid>
+
+
+
+
+
+      <Grid item xs={12} className={classes.element}>
+        <div className={classes.image}> {content}
         </div>
-        <div className={classes.image}>
-            {content}</div>
-        	<ToggleButton className={classes.comment} style={{textAlign: 'left'}}
+      </Grid>
+        
+      <Grid item xs={12} className={classes.element}>
+
+      <ToggleButton className={classes.comment} style={{textAlign: 'left'}}
       value="check"
       selected={selected}
       onChange={() => {
         setSelected(!selected);
-      }}
-    >
-        <div className={classes.identifier}>
-            Username: 
+      }}> 
+      <div className={classes.identifier}>
+            {commentuser}  
         </div>
         
-  Thanks for the photo!
-  
-    </ToggleButton>
-
-
-        <div className={classes.test}>
-        
-        
-
-        <TextField className={classes.textField}
-          placeholder="Your comment."
-          style = {{width: 450}}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="filled"
-        />
-       
-     
-      
+        <div className={classes.text}>
+      {commenttext}
         </div>
-      
-        <Button size="large" variant="contained" color="primary" className={classes.buttontest}>
+
+      </ToggleButton>
+
+      </Grid>
+
+      <Grid item xs={12} className={classes.element}>
+
+        <TextField
+              onChange={onChangeContent}
+              className={classes.newComment}
+              id="outlined-margin-none"
+              placeholder="Your Title."
+              variant="outlined"
+              InputLabelProps={{
+                shrink: true,
+              }}
+          />
+      <Button onClick={handleSubmit} size="large" variant="contained" color="primary" className={classes.post}>
             Post
         </Button>
-        
-    
-       
-    </div>
+      </Grid>
 
-
-    </div>
+    </Grid>
   );
+
+
 }
