af596b707cf7ed1ae44fb814e63bb319992f0426
sylviapas <67117878+sylviapas@users.noreply.github.com>
2020-07-08 21:25:20 +0200

    Feature/view discussion (#7)
    * added Comment Functions
    
    added createComment and addComment functions
    
    * added Voting Function for Discussions
    
    * add refresh on add comment
    
    * synchronize up- and downvote without page reload
    
    * * pass discussionId to show newly created discussion
    * make code styling in discussion and comment related components consistent
    
    Co-authored-by: Joana Becheva <ga94ted@mytum.de>

---
+++ src/services/DiscussionService.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+/**
+ * Creates discussion
+ * @param title
+ * @param content
+ * @param topic
+ * @param username
+ * @param votes
+ * @param lat
+ * @param lng
+ */
+async function createDiscussion(username, title, topic, content, votes, lat, lng) {
+    const response = await axios.post('api/discussion', {
+        "username": username,
+        "title": title,
+        "topic": topic,
+        "content": content,
+        "votes": votes,
+        "lat": lat,
+        "lng": lng
+    });
+
+    return response;
+};
+
+async function getDiscussion(discussionID) {
+    const response = await axios.get('/api/discussion/' + discussionID);
+    return response;
+}
+
+async function upVoteDiscussion(discussionID) {
+    const response = axios.put( '/api/discussion/upvote/' + discussionID);
+    return response;
+}
+
+async function downVoteDiscussion(discussionID) {
+    const response = axios.put( '/api/discussion/downvote/' + discussionID);
+    return response;
+}
+
+export {
+    createDiscussion,
+    getDiscussion,
+    upVoteDiscussion,
+    downVoteDiscussion,
+}
