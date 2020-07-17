fd1228fbf49a6df77c6c71053939bbfe0500f5a6
stefko3z <svladov.dev@gmail.com>
2020-07-17 22:10:40 +0000

    add notification new comment

--- src/components/leftmenu/Notification.js
+++ src/components/leftmenu/Notification.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { makeStyles, List, ListItem, ListItemIcon, ListItemText, ListItemSecondaryAction, IconButton } from "@material-ui/core";
 import ThumbUp from '@material-ui/icons/ThumbUp';
+import CommentIcon from '@material-ui/icons/Comment';
 import DeleteIcon from '@material-ui/icons/Delete';
+import ReportProblemIcon from '@material-ui/icons/ReportProblem';
 
 const useStyles = makeStyles((theme) => ({
     nested: {
@@ -14,19 +16,26 @@ export default function Notification(props) {
     const classes = useStyles();
 
     let primaryText = "";
+    let icon;
     switch(props.type) {
         case "disc_upvote":
             primaryText = "Discussion upvote";
+            icon = <ThumbUp />;
+            break;
+        case "comm_create":
+            primaryText = "New comment";
+            icon =  <CommentIcon />
             break;
         default:
             primaryText = "New notification";
+            icon =  <ReportProblemIcon />
             break;
     }
 
     return (
         <ListItem button className={classes.nested}>
             <ListItemIcon>
-                <ThumbUp />
+                {icon}
             </ListItemIcon>
             <ListItemText primary={primaryText} secondary={props.message}/>
             <ListItemSecondaryAction>
--- src/services/CommentService.js
+++ src/services/CommentService.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
+const config = require("./ConfigService");
+
 async function addComment(username, content, votes, discussionId) {
     const response = await axios.post('/api/comment', {
         "username": username,
         "content": content,
         "votes": votes,
         "discussionId": discussionId
+    }, {
+        headers: { Authorization: "Bearer " + config.jwtToken }
     });
 
     return response;
